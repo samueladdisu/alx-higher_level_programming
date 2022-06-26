bcebf43a4eabf1def243b339f93e939a3ad749d4
samueladdisu <samueladdisu7@gmail.com>
2022-06-26 09:08:51 +0300

    reverse be

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
-    print("{:c}".format(i if i % 2 == 0 else (i - 32)), end='')
+for i in range(122, 96, -1):
+    if i % 2 == 1:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
