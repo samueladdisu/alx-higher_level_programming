198f45b55c1333fe292290b06a4b9629ad43b18f
samueladdisu <samueladdisu7@gmail.com>
2022-07-06 20:14:57 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length < 2:
+        print("0 arguments.")
+    else:
+        if length == 2:
+            print("{} argument:".format(length - 1))
+        else:
+            print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
