14040f7372f56250f9649a55be6cad22b7c39575
samueladdisu <81099889+samueladdisu@users.noreply.github.com>
2022-09-04 19:50:08 +0300

    lazy matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
-"""
-Matrxi multiplication with numpy
-"""
+# 101-lazy_matrix_mul.py
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-
-    """
-    multiply matrix with numpy
+    """Return the multiplication of two matrices.
     Args:
-        m_a(list): first matrix
-        m_b(list): second matrix
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    if any(not isinstance(ls, list) for ls in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if any(not isinstance(ls, list) for ls in m_b):
-        raise TypeError("m_b must be a list of lists")
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-    if any(type(i) not in [int, float] for row in m_a for i in row):
-        raise TypeError("m_a should contain only integers or floats")
-    if any(type(i) not in [int, float] for row in m_b for i in row):
-        raise TypeError("m_b should contain only integers or floats")
-    if any(len(row) != len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
-    if any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
 
-    return np.matmul(m_a, m_b)
+    return (np.matmul(m_a, m_b))
