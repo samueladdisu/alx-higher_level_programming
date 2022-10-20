9f2aba584cacc8db885f4a4199189772cf9e2c63
samueladdisu <samueladdisu7@gmail.com>
2022-10-20 08:53:18 +0300

    list

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list show and genres
+SELECT ts.title, tg.name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg
+ON tg.id = tsg.genre_id
+ORDER BY ts.title, tg.name;
