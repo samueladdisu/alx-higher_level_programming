a74c884f8ff4d237ce3f367dff463f38e8e1b9c1
samueladdisu <samueladdisu7@gmail.com>
2022-10-20 05:42:25 +0300

    count

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- nuber of shows by genre
+SELECT s.name AS genre, COUNT(g.show_id) AS number_of_shows
+FROM tv_genres AS s
+INNER JOIN tv_show_genres AS g
+ON s.id = g.genre_id
+GROUP BY g.genre_id
+ORDER BY number_of_shows DESC;
