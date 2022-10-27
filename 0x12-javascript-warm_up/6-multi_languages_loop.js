0b80e9a6aa74d6e20902ea23afbb7e6a77cecc1e
samueladdisu <samueladdisu7@gmail.com>
2022-10-27 05:30:04 +0300

    exe

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-let i
-for (i = 0; i < langs.length; i++) {
+for (let i = 0; i < langs.length; i++) {
   console.log(langs[i])
 }
