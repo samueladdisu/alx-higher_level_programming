bb93dc8955534e67b97da2c94dd91f0282a22090
samueladdisu <samueladdisu7@gmail.com>
2022-10-27 05:13:56 +0300

    standard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const args = process.argv;
+const args = process.argv
 if (!args[2]) {
-	console.log('Not a number');
-}else if (isNaN(args[2])) {
-	console.log('Not a number');
-}else if (!isNaN(args[2])) {
-	console.log('My number: ', parseInt(args[2]));
+  console.log('Not a number')
+} else if (isNaN(args[2])) {
+  console.log('Not a number')
+} else if (!isNaN(args[2])) {
+  console.log('My number: ', parseInt(args[2]))
 }
-
