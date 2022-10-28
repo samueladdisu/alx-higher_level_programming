8b102ce6a65f6aa1f2d3a81f6720ddcde384d4d8
samueladdisu <samueladdisu7@gmail.com>
2022-10-28 10:08:10 +0300

    print-rectacl

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < Math.floor(Number(this.height)); i++) {
+      console.log('X'.repeat(Math.floor(Number(this.width))));
+    }
+  }
+};
